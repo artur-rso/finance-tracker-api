4813fc5bf700a795d9d5b8e977ebd1451ef739d9
Artur Rios <arturdev@duck.com>
2023-02-06 17:04:49 -0300

    feat: add password hash service

---
+++ FinanceTracker.Services/Interfaces/IPasswordHashService.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceTracker.Services.Interfaces
+{
+    public interface IPasswordHashService
+    {
+        string GetHash(string password);
+        bool IsPasswordValid(string hash, string password);
+    }
+}
---
+++ FinanceTracker.Services/PasswordHashService.cs
@@ -0,0 +1,18 @@
+﻿using FinanceTracker.Services.Interfaces;
+
+namespace FinanceTracker.Services
+{
+    public class PasswordHashService : IPasswordHashService
+    {
+        public string GetHash(string password)
+        {
+            // TODO: hash passwords using a random salt, after concatenating the password with a pepper
+            throw new NotImplementedException();
+        }
+
+        public bool IsPasswordValid(string hash, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
